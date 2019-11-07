2ee942939c41a5dce8488846987478e8a06f7274
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-11-06 21:09:48 -0500

    function w/if-else statement test if strings match

---
+++ test-if-strings-match.js
@@ -0,0 +1,22 @@
+
+wordChoice = ["wholeness", "balance", "universe"];
+let wordSelected = wordChoice[1];
+let wordTest = "balances";
+let result;
+//console.log(wordSelected);
+
+function testingWords(result){
+  if(wordSelected === wordTest){
+  return true;
+  } else {
+  return false;
+  }
+}
+
+var output = testingWords(result);
+console.log(output);
+
+  
+
+
+  
